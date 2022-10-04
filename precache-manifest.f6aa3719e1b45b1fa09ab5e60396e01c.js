c5c4d7c2bafbde597acd6c6f8b40657bd98f2863
yangxdev <j.xiang@campus.unimib.it>
2022-10-05 01:03:26 +0200

    Updates

---
+++ precache-manifest.f6aa3719e1b45b1fa09ab5e60396e01c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "0bbf4d3f9e40ca08faf3",
+    "url": "/static/css/main.39ca0214.chunk.css"
+  },
+  {
+    "revision": "0bbf4d3f9e40ca08faf3",
+    "url": "/static/js/main.0bbf4d3f.chunk.js"
+  },
+  {
+    "revision": "7f8c91ddc4b58ecb5b87",
+    "url": "/static/js/1.7f8c91dd.chunk.js"
+  },
+  {
+    "revision": "bb147b80d1b217ff1a88",
+    "url": "/static/js/2.bb147b80.chunk.js"
+  },
+  {
+    "revision": "fa1e30c6e6c64559af32",
+    "url": "/static/js/runtime~main.fa1e30c6.js"
+  },
+  {
+    "revision": "ea61094180cd06f40a5956af5191646d",
+    "url": "/static/media/icon-cart.ea610941.svg"
+  },
+  {
+    "revision": "22a9013809b6b2d6f8a242b9a835787e",
+    "url": "/static/media/image-product-desktop.22a90138.jpg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "2a3dafad1295389827a355b117410516",
+    "url": "/index.html"
+  }
+];
